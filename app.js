6e553a3043c8d560ae9782694bda6028a6dde9b8
fpontes006 <fpontes006@gmail.com>
2021-07-20 10:36:56 -0300

    Processor
    processando dados

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 var Reader = require("./Reader");
+var Processor = require("./Processor");
+
 
 var leitor = new Reader();
 
 async function main(){
 
    var dados = await leitor.read("./excel.csv");
-   console.log(dados);
+  var dadosProcesados =  Processor.Process(dados);
 }
 
 main();
